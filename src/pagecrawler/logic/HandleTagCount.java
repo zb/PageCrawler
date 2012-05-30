75ee6b75d4bd197303e20f6661a8d4dafe2033b3
zb <zlbjux@gmail.com>
2012-05-29 17:43:03 -0700

    Update src/pagecrawler/logic/HandleTagCount.java

--- src/pagecrawler/logic/HandleTagCount.java
+++ src/pagecrawler/logic/HandleTagCount.java
@@ -7,7 +7,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class HandleTagCount 
 {	
-	static String[] allHtmlTags = {"<address", "<applet", "<area", "<a ", "<base", "<basefont", "<big", "<blockquote", "<body", "<br", "<b>", "<caption", "<center",
+	static String[] allHtmlTags = {"<address", "<applet", "<area", "<a ", "<base>", "<basefont", "<big", "<blockquote", "<body", "<br", "<b>", "<caption", "<center",
 			"<cite", "<code", "<dd", "<dfn", "<dir", "<div", "<dl", "<dt", "<em", "<font", "<form", "<h1", "<h2", "<h3", "<h4", "<h5", "<h6", "<head", "<hr",
 			"<html", "<img", "<input", "<isindex", "<i>", "<kbd", "<link", "<li>", "<map", "<menu", "<meta", "<ol", "<option", "<param", "<pre", "<p>", "<samp",
 			"<script", "<select", "<small", "<strike", "<strong", "<style", "<sub", "<sup", "<table", "<td", "<textarea", "<th", "<title", "<tr", "<tt", "<ul", "<u>", "<var"};  
