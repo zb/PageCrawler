c55b15891f725930f818cdb056171610b4ca0aef
zb <zlbjux@gmail.com>
2012-06-09 15:24:07 -0700

    Update master

--- src/pagecrawler/logic/HandleTagCount.java
+++ src/pagecrawler/logic/HandleTagCount.java
@@ -7,10 +7,10 @@ import org.apache.commons.lang3.StringUtils;
 
 public class HandleTagCount 
 {	
-static String[] allHtmlTags = { "<a ","<abbr", "<address", "<area", "<article", "<aside", "<audio", "<b>", "<base>", "<bdi", "<bdo", "<blockquote", "<body", "<br", 
+	static String[] allHtmlTags = { "<a ","<abbr", "<address", "<area", "<article", "<aside", "<audio", "<b>", "<base>", "<bdi", "<bdo", "<blockquote", "<body", "<br", 
 		"<button", "<canvas", "<caption", "<cite", "<code", "<col>", "<colgroup", "<command", "<datalist", "<dd", "<del", "<details", "<dfn", "<div", "<dl", 
-		"<dt", "<em", "<embed", "<fieldset","<figcaption", "figure", "<footer", "<form", "<h1", "<h2", "<h3", "<h4", "<h5", "<h6", "<head>", "<header", "<hrgroup",
-		"<hr", "<html", "<i>", "<iframe", "<img", "<input", "<ins", "<keygen", "<kbd", "<label", "<legend", "<link", "<li>", "<map", "<mark", "<menu", "<meta", "<meter",
+		"<dt", "<em>", "<embed", "<fieldset","<figcaption", "<figure", "<footer", "<form", "<h1", "<h2", "<h3", "<h4", "<h5", "<h6", "<head>", "<header", "<hrgroup",
+		"<hr>", "<html", "<i>", "<iframe", "<img", "<input", "<ins", "<keygen", "<kbd", "<label", "<legend", "<link", "<li>", "<map", "<mark", "<menu", "<meta", "<meter",
 		"<nav", "<noscript", "<object", "<ol", "<optgroup", "<option", "<output", "<p>", "<param", "<pre", "<progress", "<q", "<rp", "<rt", "<ruby", "<s>", "<samp",
 		"<script", "<select", "<small", "<source", "<span", "<strong", "<style", "<sub", "<summary", "<sup", "<table", "<tbody", "<td", "<textarea", "<tfoot", "<th>", 
 		"<thead", "<time", "<title", "<tr>", "<track", "<u>", "<ul", "<var", "<video", "<wbr"};  
